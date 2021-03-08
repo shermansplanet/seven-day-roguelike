c6e594114292ae0985ecb6ead14227dc8de08a92
Loren Sherman <lsherman@mit.edu>
2021-03-08 13:36:01 -0800

    draggable grid

---
+++ Assets/Gameplay/CardInstance.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardInstance : MonoBehaviour
+{
+    private ConversationGrid grid;
+
+    // How many pixels tall a grid square is
+    private float gridSquarePixels;
+    private Vector3 originalMousePosition;
+    private Vector3 originalCardPosition;
+
+    [HideInInspector]
+    public int x, y;
+
+    public void Init(ConversationGrid grid)
+    {
+        this.grid = grid;
+        gridSquarePixels = Screen.height / grid.GridSquaresVertical;
+        transform.SetParent(grid.transform);
+        transform.localScale = Vector3.one;
+    }
+
+    private void OnMouseDown()
+    {
+        originalMousePosition = Input.mousePosition;
+        originalCardPosition = transform.localPosition;
+    }
+
+    private void OnMouseDrag()
+    {
+        x = Mathf.RoundToInt((Input.mousePosition.x - Screen.width / 2) / gridSquarePixels);
+        y = Mathf.RoundToInt((Input.mousePosition.y - Screen.height / 2) / gridSquarePixels);
+
+        transform.localPosition = originalCardPosition +
+            (Input.mousePosition - originalMousePosition) / gridSquarePixels;
+
+        grid.OnCardDrag(this);
+    }
+
+    private void OnMouseUp()
+    {
+        grid.OnCardRelease(this);
+    }
+
+    public void CancelMove()
+    {
+        transform.position = originalCardPosition;
+    }
+}
---
+++ Assets/Gameplay/ConversationGrid.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConversationGrid : MonoBehaviour
+{
+    // How many grid squares fit into the screen vertically
+    public float GridSquaresVertical = 10f;
+    public CardInstance cardPrefab;
+    public SpriteRenderer sprite;
+    public Transform highlight;
+
+    [HideInInspector]
+    public float gridScale;
+
+    private List<CardInstance> cards = new List<CardInstance>();
+
+    void Start()
+    {
+        gridScale = Camera.main.orthographicSize * 2 / GridSquaresVertical;
+        transform.localScale = Vector3.one * gridScale;
+
+        CardInstance card = Instantiate(cardPrefab);
+        cards.Add(card);
+        card.Init(this);
+    }
+
+    public void OnCardRelease(CardInstance card)
+    {
+        card.transform.localPosition = new Vector3(card.x, card.y, 0);
+    }
+
+    public void OnCardDrag(CardInstance card)
+    {
+        highlight.localPosition = new Vector3(card.x, card.y, 0);
+    }
+
+    // Not optimal but like how many cards are gonna be on the field at once
+    public CardInstance GetCard(int x, int y)
+    {
+        foreach(CardInstance card in cards)
+        {
+            if(card.x == x && card.y == y)
+            {
+                return card;
+            }
+        }
+
+        return null;
+    }
+}
