3ecf3219eca5a58a1da6ee5cc87788a5591e6d36
Loren Sherman <lsherman@mit.edu>
2021-03-07 23:37:27 -0800

    card list

---
+++ Assets/Data/CardList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardList : MonoBehaviour
+{
+    public enum CardID
+    {
+        EXAMPLE
+    }
+
+    [Serializable]
+    public struct CardData
+    {
+        public CardID id;
+        public string name;
+    }
+
+    public CardData[] cards;
+}
