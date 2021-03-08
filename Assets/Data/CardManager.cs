b64f9c078d6a906446e80a766b60852f690a9ae2
Tara Liu <taraliu@mit.edu>
2021-03-08 00:47:25 -0800

    add inventory and cards

---
+++ Assets/Data/CardManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardManager : MonoBehaviour
+{
+    public enum CardId
+    {
+        EXAMPLE
+    }
+
+    [Serializable]
+    public struct CardData
+    {
+        public CardId id;
+        public string name;
+    }
+
+    public CardData[] cards;
+    static Dictionary<CardId, CardData> cardIdToData;
+
+    private void Awake() {
+        cardIdToData = new Dictionary<CardId, CardData>();
+        foreach (CardData card in cards) {
+            cardIdToData[card.id] = card;
+        }
+    }
+
+    public static CardData GetDataById(CardId id) {
+        return cardIdToData[id];
+    }
+}
