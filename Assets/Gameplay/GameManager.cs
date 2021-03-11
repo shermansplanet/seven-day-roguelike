ce367d02fc78996c81da165ece119dee4fe4db75
Loren Sherman <lsherman@mit.edu>
2021-03-11 13:35:49 -0800

    overworld placeholder and persistent inventory

--- Assets/Gameplay/GameManager.cs
+++ Assets/Gameplay/GameManager.cs
@@ -1,22 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    private const int ScoreToWin = 10;
+
     public InventoryUI inventoryUI;
     public ConversationGrid grid;
     public static bool playerTurn = true;
+    public static int score;
 
     private NPC npc;
+
+    private static Inventory inventory;
 
     // Start is called before the first frame update
     void Start()
     {
-        Inventory inventory = new Inventory();
-        inventory.PopulateInventory();
+        if (inventory == null)
+        {
+            inventory = new Inventory();
+            inventory.PopulateInventory();
+        }
         inventoryUI.Init(inventory);
         npc = new NPC();
+        score = 0;
     }
 
     public void OnConfirm()
@@ -24,12 +34,23 @@ public class GameManager : MonoBehaviour
         StartCoroutine(ConfirmLoop());
     }
 
-    public IEnumerator ConfirmLoop()
+    public void OnCardPlaced()
     {
         grid.ConfirmMove();
+        if(score >= ScoreToWin)
+        {
+            SceneManager.LoadScene(0);
+            StopAllCoroutines();
+        }
+    }
+
+    public IEnumerator ConfirmLoop()
+    {
+        OnCardPlaced();
         playerTurn = false;
         yield return new WaitForSeconds(1);
         npc.TakeTurn(grid);
+        OnCardPlaced();
         playerTurn = true;
         inventoryUI.DrawHand();
     }
